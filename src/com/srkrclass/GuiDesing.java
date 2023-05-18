34c5434fcc4774ddbe215fbfc5d05a6ef66ff5ff
PRSSVenkatapahiRaju <prssvraju2@gmail.com>
2023-05-18 07:28:35 +0530

    uupdate

--- src/com/srkrclass/GuiDesing.java
+++ src/com/srkrclass/GuiDesing.java
@@ -2,9 +2,7 @@ package com.srkrclass;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-
 public class GuiDesing extends JFrame implements ActionListener,ItemListener{
-
 	JRadioButton radio;
 	JCheckBox check;
 	JLabel l;
@@ -20,9 +18,14 @@ public class GuiDesing extends JFrame implements ActionListener,ItemListener{
 	JMenu mainmenu;
 	JMenu submenu;
 	JMenuItem jmi1,jmi2,jmi3,jmi4;
-	
+	JButton click;
 	GuiDesing()
 	{
+		setSize(700,700);
+		setVisible(true);
+		setLayout(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		radio = new JRadioButton("IT");
 		radio.setBounds(40,10,100,20);
 		check = new JCheckBox("Cse");
@@ -34,73 +37,73 @@ public class GuiDesing extends JFrame implements ActionListener,ItemListener{
 		
 	
 		
-//		ta =new JTextArea(100,100);
-//		p1 = new JPanel();
-//		p1.add(ta);
-//		
-//		p2 = new JPanel();
-//		p3 = new JPanel();
-//		
-//		jtp = new JTabbedPane();
-//		jtp.setBounds(40,120,200,200);
-//		jtp.add("Main",p1);
-//		jtp.add("Edit",p2);
-//		jtp.add("Help",p3);
+		ta =new JTextArea(100,100);
+		p1 = new JPanel();
+		p1.add(ta);
 		
-		l = new JLabel("Message");
-		l.setBounds(40, 320, 100,20);
+		p2 = new JPanel();
+		p3 = new JPanel();
 		
+		jtp = new JTabbedPane();
+		jtp.setBounds(40,120,200,100);
+		jtp.add("Main",p1);
+		jtp.add("Edit",p2);
+		jtp.add("Help",p3);
+
 		
 		ta= new JTextArea(100, 100);
 		jsp = new JScrollPane(ta);
-		jsp.setBounds(40, 150, 100,100);
+		jsp.setBounds(40, 220, 100,100);
 		jsp.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		
 		
 		String weeks[]= {"Sun","Mon","Tues"};
 		jl= new JList(weeks);
 		jl.setSelectedIndex(1);
-		jl.setBounds(40, 260, 100, 100);
+		jl.setBounds(40, 340, 100, 100);
 		
 		jcb = new JComboBox(weeks);
-		jcb.setBounds(40, 330, 100, 100);
-		
+		jcb.setBounds(40, 430, 100, 100);
 		
 		jmb = new JMenuBar();
-		
-		
+		mainmenu=new JMenu("Menu");  
+        submenu=new JMenu("Sub Menu");  
+        jmi1=new JMenuItem("Item 1");  
+        jmi2=new JMenuItem("Item 2");  
+        jmi3=new JMenuItem("Item 3");  
+        jmi4=new JMenuItem("Item 4");  
+        
+        mainmenu.add(jmi1); 
+        mainmenu.add(jmi2);
+        submenu.add(jmi3);  
+        submenu.add(jmi4);  
+        mainmenu.add(submenu);  
+        
+        jmb.add(mainmenu);  
+        setJMenuBar(jmb); 
 
-		
-		
-
+        click = new JButton("Click");
+        click.setBounds(40,530,100,20);
+        
+        l = new JLabel("Message");
+        l.setBounds(40,550,100,40);
 		
 		radio.addItemListener(this);
-		check.addActionListener(this);
 		jtb.addItemListener(this);
-
+		check.addActionListener(this);
 
 		add(radio);
 		add(check);	
 		add(tf);
 		add(jtb);
+		add(jtp);
 		add(jsp);
 		add(jl);
 		add(jcb);
-		add(l);
-		
-		
-		
-		setSize(500,500);
-		setVisible(true);
-		setLayout(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		add(click);
+		add(l);		
 	}
 	
-	
-	@Override
 	public void itemStateChanged(ItemEvent e) {
-		// TODO Auto-generated method stub
 		if(e.getSource() == radio)
 		{
 			if(radio.isSelected())
@@ -124,10 +127,7 @@ public class GuiDesing extends JFrame implements ActionListener,ItemListener{
 			}
 		}
 	}
-
-	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		
 		if(check.isSelected())
 		{
@@ -137,7 +137,6 @@ public class GuiDesing extends JFrame implements ActionListener,ItemListener{
 		{
 			l.setText("Cse is Not selected");
 		}
-		
 	}
 	public static void main(String args[])
 	{
